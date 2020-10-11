220758a031a7a7fa3b39f36f5c01a842bcfdd5f4
Jonny Frey <jonathan.frey@hgdata.com>
2020-10-10 23:07:05 -0700

    Need that backend url

--- sandbox-frontend/vue.config.js
+++ sandbox-frontend/vue.config.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV === "production") {
+  process.env.VUE_APP_BACKEND_URL =
+    "https://0v3ak91mi9.execute-api.us-west-2.amazonaws.com/Prod";
+}
+
 module.exports = {
   transpileDependencies: ["vuetify"]
 };
